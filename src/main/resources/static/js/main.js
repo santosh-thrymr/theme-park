15098d992fcbc63691933942440449aa32f783c6
tantan-thrymr <tantan.kumar@thrymr.net>
2018-03-13 18:38:14 +0530

    done calculation for all field

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -58,13 +58,247 @@ $(document).ready(function() {
 		if(attrValue!='' && value!=''){
 			calculatedValue=attrValue*value;
 		$(this).closest('td').next('td').find('input').val(calculatedValue);
-		console.log("vall",value);
 		}else if(value==''){
 			$(this).closest('td').next('td').find('input').val('');
 		}
 		
 	
 	});
+	$(document).on('keyup', '.annualPass', function() {
+		var attrValue = $(this).attr('data-attr');
+		var value = $(this).val();
+		var calculatedValue;
+		if(attrValue!='' && value!=''){
+			calculatedValue=attrValue*value;
+		$(this).closest('td').next('td').find('input').val(calculatedValue);
+		}else if(value==''){
+			$(this).closest('td').next('td').find('input').val('');
+		}
+		
 	
-});
+	});
+	$(document).on('keyup', '.single-entry', function() {
+		var myk = $(this).attr('myk');
+		var std = $(this).attr('std');
+		var value = $(this).val();
+		var nationality=$(".nationality").val();
+		var calculatedValue;
+		if((myk!='' || std!='') && value!=''){
+			if(nationality !='' && nationality=='malaysian'){
+				calculatedValue=myk*value;
+			} else {
+				calculatedValue=std*value;
+			}
+			
+		$(this).closest('td').next('td').find('input').val(calculatedValue);
+		}else if(value==''){
+			$(this).closest('td').next('td').find('input').val('');
+		}
+		
+	
+	});
+	$(document).on('change', '.nationality', function() {
+		$(".single-entry").each(function(){
+			var myk = $(this).attr('myk');
+			var std = $(this).attr('std');
+			var value = $(this).val();
+			var nationality=$(".nationality").val();
+			var calculatedValue;
+			if((myk!='' || std!='') && value!=''){
+				if(nationality !='' && nationality=='malaysian'){
+					calculatedValue=myk*value;
+				} else {
+					calculatedValue=std*value;
+				}
+				
+			$(this).closest('td').next('td').find('input').val(calculatedValue);
+			}else if(value==''){
+				$(this).closest('td').next('td').find('input').val('');
+			}
+		});
+		
+		
+		//BigLondal Admission fee
+		$(".adult-qua").each(function(){
+			var myk = $(this).attr('myk');
+			var std = $(this).attr('std');
+			var value = $(this).val();
+			var nextmyk=$(this).closest('td').next('td').find('input').attr('myk-kid');
+			var nextstd=$(this).closest('td').next('td').find('input').attr('std-kid');
+			var nextvalue=$(this).closest('td').next('td').find('input').val();
+			var nationality=$(".nationality").val();
+			console.log("nationality",nationality);
+			var calculatedValue;
+			var nextcalculatedValue;
+			if((myk!='' || std!='') && value!=''){
+				if(nationality !='' && nationality=='malaysian'){
+					calculatedValue=myk*value;
+				} else {
+					calculatedValue=std*value;
+				}
+			}
+			if((nextmyk!='' || nextstd!='') && nextvalue!=''){
+				if(nationality !='' && nationality=='malaysian'){
+					nextcalculatedValue=nextmyk*nextvalue;
+				} else {
+					nextcalculatedValue=nextstd*nextvalue;
+				}
+			}
+			var finalCalculation;
+			if(nextcalculatedValue!=null && calculatedValue!=null){
+				finalCalculation=nextcalculatedValue+calculatedValue;			
+			}else if(nextcalculatedValue!=null && calculatedValue==null){
+				finalCalculation=nextcalculatedValue;
+			}else{
+				finalCalculation=calculatedValue;
+			}
+			
+			if(finalCalculation!=''){
+				$(this).closest('td').next('td').next('td').find('input').val(finalCalculation);
+			}
+			else{
+				$(this).closest('td').next('td').next('td').find('input').val('');
+			}
+		});
+		
+		$(".kids-qua").each(function(){
+			var myk = $(this).attr('myk-kid');
+			var std = $(this).attr('std-kid');
+			var value = $(this).val();
+			var prevmyk=$(this).closest('td').prev('td').find('input').attr('myk');
+			var prevstd=$(this).closest('td').prev('td').find('input').attr('std');
+			var prevvalue=$(this).closest('td').prev('td').find('input').val();
+			var nationality=$(".nationality").val();
+			var calculatedValue;
+			var prevcalculatedValue;
+			if((myk!='' || std!='') && value!=''){
+				if(nationality !='' && nationality=='malaysian'){
+					calculatedValue=myk*value;
+				} else {
+					calculatedValue=std*value;
+				}
+			}
+			if((prevmyk!='' || prevstd!='') && prevvalue!=''){
+				if(nationality !='' && nationality=='malaysian'){
+					
+					prevcalculatedValue=prevmyk*prevvalue;
+				} else {
+					prevcalculatedValue=prevstd*prevvalue;
+				}
+			}
+			var finalCalculation;
+			if(prevcalculatedValue!=null && calculatedValue!=null){
+				finalCalculation=prevcalculatedValue+calculatedValue;
+				
+			}else if(prevcalculatedValue!=null && calculatedValue==null){
+				finalCalculation=prevcalculatedValue;
+				
+			}else{
+				finalCalculation=calculatedValue;
+			}
+			
+			if(finalCalculation!=''){
+				$(this).closest('td').next('td').find('input').val(finalCalculation);
+			}
+			else{
+				$(this).closest('td').next('td').find('input').val('');
+			}
+			
+		});
+		
+      
+	});
+	
+	$(document).on('keyup', '.adult-qua', function() {
+		var myk = $(this).attr('myk');
+		var std = $(this).attr('std');
+		var value = $(this).val();
+		var nextmyk=$(this).closest('td').next('td').find('input').attr('myk-kid');
+		var nextstd=$(this).closest('td').next('td').find('input').attr('std-kid');
+		var nextvalue=$(this).closest('td').next('td').find('input').val();
+		var nationality=$(".nationality").val();
+		var calculatedValue;
+		var nextcalculatedValue;
+		if((myk!='' || std!='') && value!=''){
+			if(nationality !='' && nationality=='malaysian'){
+				calculatedValue=myk*value;
+			} else {
+				calculatedValue=std*value;
+			}
+		}
+		if((nextmyk!='' || nextstd!='') && nextvalue!=''){
+			if(nationality !='' && nationality=='malaysian'){
+				nextcalculatedValue=nextmyk*nextvalue;
+			} else {
+				nextcalculatedValue=nextstd*nextvalue;
+			}
+		}
+		var finalCalculation;
+		if(nextcalculatedValue!=null && calculatedValue!=null){
+			finalCalculation=nextcalculatedValue+calculatedValue;			
+		}else if(nextcalculatedValue!=null && calculatedValue==null){
+			finalCalculation=nextcalculatedValue;
+		}else{
+			finalCalculation=calculatedValue;
+		}
+		
+		if(finalCalculation!=''){
+			$(this).closest('td').next('td').next('td').find('input').val(finalCalculation);
+		}
+		else{
+			$(this).closest('td').next('td').next('td').find('input').val('');
+		}
+	
+	});
+	$(document).on('keyup', '.kids-qua', function() {
+		var myk = $(this).attr('myk-kid');
+		var std = $(this).attr('std-kid');
+		var value = $(this).val();
+		var prevmyk=$(this).closest('td').prev('td').find('input').attr('myk');
+		var prevstd=$(this).closest('td').prev('td').find('input').attr('std');
+		var prevvalue=$(this).closest('td').prev('td').find('input').val();
+		var nationality=$(".nationality").val();
+		var calculatedValue;
+		var prevcalculatedValue;
+		if((myk!='' || std!='') && value!=''){
+			if(nationality !='' && nationality=='malaysian'){
+				calculatedValue=myk*value;
+			} else {
+				calculatedValue=std*value;
+			}
+		}
+		if((prevmyk!='' || prevstd!='') && prevvalue!=''){
+			if(nationality !='' && nationality=='malaysian'){
+				
+				prevcalculatedValue=prevmyk*prevvalue;
+			} else {
+				prevcalculatedValue=prevstd*prevvalue;
+			}
+		}
+		var finalCalculation;
+		if(prevcalculatedValue!=null && calculatedValue!=null){
+			finalCalculation=prevcalculatedValue+calculatedValue;
+			
+		}else if(prevcalculatedValue!=null && calculatedValue==null){
+			finalCalculation=prevcalculatedValue;
+			
+		}else{
+			finalCalculation=calculatedValue;
+		}
+		
+		if(finalCalculation!=''){
+			$(this).closest('td').next('td').find('input').val(finalCalculation);
+		}
+		else{
+			$(this).closest('td').next('td').find('input').val('');
+		}
+	
+	});
+	
+	$(document).on('change', '.nationality', function() {
+		
+
+	});
+	
+         });
 // ]]>
